320785e8681c6fe8ebb018d88964a9eccdb2c5d7
unknown <adrianapaivausa@icloud.com>
2024-10-11 17:30:08 -0400

    working on cards

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 
 
-console.clear();
+// console.clear();
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-window.addEventListener("load", () => {
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: ".wrapper",
-        start: "top top",
-        end: "+=150%",
-        pin: true,
-        scrub: true,
-        markers: false
-      }
-    })
-    .to("img", {
-      scale: 2,
-      z: 350,
-      transformOrigin: "center center",
-      ease: "power1.inOut"
-    })
-    .to(
-      ".section-hero",
-      {
-        scale: 1.1,
-        transformOrigin: "center center",
-        ease: "power1.inOut"
-      },
-      "<"
-    );
-});
+// window.addEventListener("load", () => {
+//   gsap
+//     .timeline({
+//       scrollTrigger: {
+//         trigger: ".hero-content",
+//         start: "top top",
+//         end: "+=150%",
+//         pin: true,
+//         scrub: 1,
+//         markers: false
+//       }
+//     })
+//     .to("img", {
+//       scale: 2,
+//       z: 350,
+//       transformOrigin: "center center",
+//       ease: "power1.inOut"
+//     })
+//     .to(
+//       ".section-hero",
+//       {
+//         scale: 1.1,
+//         transformOrigin: "center center",
+//         ease: "power1.inOut"
+//       },
+//       "<"
+//     );
+// });
+
 
 const navToggle = document.getElementById('navToggle');
 
