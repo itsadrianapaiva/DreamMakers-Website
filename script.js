7c12bcdca0aae4d6aab93c46e620d7e89248cdea
unknown <adrianapaivausa@icloud.com>
2024-11-19 15:27:39 -0500

    under construction page added

--- script.js
+++ script.js
@@ -1,39 +1,5 @@
 
 
-// console.clear();
-
-// gsap.registerPlugin(ScrollTrigger);
-
-// window.addEventListener("load", () => {
-//   gsap
-//     .timeline({
-//       scrollTrigger: {
-//         trigger: ".hero-content",
-//         start: "top top",
-//         end: "+=150%",
-//         pin: true,
-//         scrub: 1,
-//         markers: false
-//       }
-//     })
-//     .to("img", {
-//       scale: 2,
-//       z: 350,
-//       transformOrigin: "center center",
-//       ease: "power1.inOut"
-//     })
-//     .to(
-//       ".section-hero",
-//       {
-//         scale: 1.1,
-//         transformOrigin: "center center",
-//         ease: "power1.inOut"
-//       },
-//       "<"
-//     );
-// });
-
-
 const navToggle = document.getElementById('navToggle');
 
 
